29a25400abb397b1e3e87256afe006f4e131c85d
Thomas Borg Salling <tbsalling@tbsalling.dk>
2017-04-23 08:49:27 +0200

    Spring Boot app created by Spring Initializr

---
+++ src/main/java/dk/tbsalling/ais/PlotterApplication.java
@@ -0,0 +1,12 @@
+package dk.tbsalling.ais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlotterApplication.class, args);
+	}
+}
---
+++ src/test/java/dk/tbsalling/ais/PlotterApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.tbsalling.ais;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlotterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
